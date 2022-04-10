46229130cb29c53971811b152a72e425d8509cad
sevenight <emin.bilgen@outlook.com.tr>
2022-04-10 18:43:26 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="portfolyos">
+        <div class="bar">
+            <span class="cross"></span>
+            <span class="crossx"></span>
+            <span class="crossy"></span>
+        </div>
+        <h2 class="title">We are under maintence!!</h2>
+        
+        
+        
+        <p class="copyright">© Bilgen | Since 2021</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
